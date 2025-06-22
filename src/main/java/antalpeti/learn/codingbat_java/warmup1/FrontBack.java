21939de66b2618d91c645ae700deb93a1475dbe8
antalpeti <antalpeti@users.noreply.github.com>
2025-06-22 04:48:27 +0200

    Add FrontBack class and corresponding unit tests for string manipulation

---
+++ src/main/java/antalpeti/learn/codingbat_java/warmup1/FrontBack.java
@@ -0,0 +1,13 @@
+package antalpeti.learn.codingbat_java.warmup1;
+
+public class FrontBack {
+  public String frontBack(String str) {
+    if (str.length() <= 1) {
+      return str;
+    }
+    char first = str.charAt(0);
+    char last = str.charAt(str.length() - 1);
+    String middle = str.substring(1, str.length() - 1);
+    return last + middle + first;
+  }
+}
---
+++ src/test/java/antalpeti/learn/codingbat_java/warmup1/FrontBackTest.java
@@ -0,0 +1,50 @@
+package antalpeti.learn.codingbat_java.warmup1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FrontBackTest {
+
+  private final FrontBack frontBack = new FrontBack();
+
+  @Test
+  void testFrontBack_code() {
+    assertEquals("eodc", frontBack.frontBack("code"));
+  }
+
+  @Test
+  void testFrontBack_a() {
+    assertEquals("a", frontBack.frontBack("a"));
+  }
+
+  @Test
+  void testFrontBack_ab() {
+    assertEquals("ba", frontBack.frontBack("ab"));
+  }
+
+  @Test
+  void testFrontBack_abc() {
+    assertEquals("cba", frontBack.frontBack("abc"));
+  }
+
+  @Test
+  void testFrontBack_empty() {
+    assertEquals("", frontBack.frontBack(""));
+  }
+
+  @Test
+  void testFrontBack_Chocolate() {
+    assertEquals("ehocolatC", frontBack.frontBack("Chocolate"));
+  }
+
+  @Test
+  void testFrontBack_aavJ() {
+    assertEquals("Java", frontBack.frontBack("aavJ"));
+  }
+
+  @Test
+  void testFrontBack_hello() {
+    assertEquals("oellh", frontBack.frontBack("hello"));
+  }
+}
