c7756b60e6defe6e2e99d240f2b1d7921638689d
antalpeti <antalpeti@gmail.com>
2025-06-22 03:23:50 +0200

    Refactor test method names for clarity and add NearHundred class with tests

--- src/test/java/antalpeti/learn/codingbat_java/warmup1/Diff21Test.java
+++ src/test/java/antalpeti/learn/codingbat_java/warmup1/Diff21Test.java
@@ -11,62 +11,62 @@ class Diff21Test {
     Diff21 diff21;
 
     @Test
-    void testDiff21_19() {
+    void testDiff21_whenInputIs19() {
         assertEquals(2, diff21.diff21(19));
     }
 
     @Test
-    void testDiff21_10() {
+    void testDiff21_whenInputIs10() {
         assertEquals(11, diff21.diff21(10));
     }
 
     @Test
-    void testDiff21_21() {
+    void testDiff21_whenInputIs21() {
         assertEquals(0, diff21.diff21(21));
     }
 
     @Test
-    void testDiff21_22() {
+    void testDiff21_whenInputIs22() {
         assertEquals(2, diff21.diff21(22));
     }
 
     @Test
-    void testDiff21_25() {
+    void testDiff21_whenInputIs25() {
         assertEquals(8, diff21.diff21(25));
     }
 
     @Test
-    void testDiff21_30() {
+    void testDiff21_whenInputIs30() {
         assertEquals(18, diff21.diff21(30));
     }
 
     @Test
-    void testDiff21_0() {
+    void testDiff21_whenInputIs0() {
         assertEquals(21, diff21.diff21(0));
     }
 
     @Test
-    void testDiff21_1() {
+    void testDiff21_whenInputIs1() {
         assertEquals(20, diff21.diff21(1));
     }
 
     @Test
-    void testDiff21_2() {
+    void testDiff21_whenInputIs2() {
         assertEquals(19, diff21.diff21(2));
     }
 
     @Test
-    void testDiff21_minus1() {
+    void testDiff21_whenInputIsMinus1() {
         assertEquals(22, diff21.diff21(-1));
     }
 
     @Test
-    void testDiff21_minus2() {
+    void testDiff21_whenInputIsMinus2() {
         assertEquals(23, diff21.diff21(-2));
     }
 
     @Test
-    void testDiff21_50() {
+    void testDiff21_whenInputIs50() {
         assertEquals(58, diff21.diff21(50));
     }
 }
--- src/test/java/antalpeti/learn/codingbat_java/warmup1/SumDoubleTest.java
+++ src/test/java/antalpeti/learn/codingbat_java/warmup1/SumDoubleTest.java
@@ -11,42 +11,42 @@ class SumDoubleTest {
     SumDouble sumDouble;
 
     @Test
-    void testSumDouble_1_2() {
+    void testSumDouble_whenInputIs1And2() {
         assertEquals(3, sumDouble.sumDouble(1, 2));
     }
 
     @Test
-    void testSumDouble_3_2() {
+    void testSumDouble_whenInputIs3And2() {
         assertEquals(5, sumDouble.sumDouble(3, 2));
     }
 
     @Test
-    void testSumDouble_2_2() {
+    void testSumDouble_whenInputIs2And2() {
         assertEquals(8, sumDouble.sumDouble(2, 2));
     }
 
     @Test
-    void testSumDouble_minus1_0() {
+    void testSumDouble_whenInputIsMinus1And0() {
         assertEquals(-1, sumDouble.sumDouble(-1, 0));
     }
 
     @Test
-    void testSumDouble_3_3() {
+    void testSumDouble_whenInputIs3And3() {
         assertEquals(12, sumDouble.sumDouble(3, 3));
     }
 
     @Test
-    void testSumDouble_0_0() {
+    void testSumDouble_whenInputIs0And0() {
         assertEquals(0, sumDouble.sumDouble(0, 0));
     }
 
     @Test
-    void testSumDouble_0_1() {
+    void testSumDouble_whenInputIs0And1() {
         assertEquals(1, sumDouble.sumDouble(0, 1));
     }
 
     @Test
-    void testSumDouble_3_4() {
+    void testSumDouble_whenInputIs3And4() {
         assertEquals(7, sumDouble.sumDouble(3, 4));
     }
 }
