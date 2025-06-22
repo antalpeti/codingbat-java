05b626d3082b6c20a66fc984f91017fc4be7e9b4
antalpeti <antalpeti@gmail.com>
2025-06-22 03:40:31 +0200

    Refactor test imports for consistency and add PosNeg class with tests

---
+++ src/main/java/antalpeti/learn/codingbat_java/warmup1/PosNeg.java
@@ -0,0 +1,14 @@
+package antalpeti.learn.codingbat_java.warmup1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PosNeg {
+  public boolean posNeg(int a, int b, boolean negative) {
+    if (negative) {
+      return a < 0 && b < 0;
+    } else {
+      return (a < 0 && b > 0) || (a > 0 && b < 0);
+    }
+  }
+}
--- src/test/java/antalpeti/learn/codingbat_java/warmup1/SumDoubleTest.java
+++ src/test/java/antalpeti/learn/codingbat_java/warmup1/SumDoubleTest.java
@@ -1,9 +1,10 @@
 package antalpeti.learn.codingbat_java.warmup1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SumDoubleTest {
