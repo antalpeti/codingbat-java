298ab0b3164d2bbfbe869a34093c180734cc0848
antalpeti <antalpeti@users.noreply.github.com>
2025-06-22 02:05:47 +0200

    Add ParrotTrouble class and corresponding unit tests

---
+++ src/main/java/antalpeti/learn/codingbat_java/warmup1/ParrotTrouble.java
@@ -0,0 +1,10 @@
+package antalpeti.learn.codingbat_java.warmup1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ParrotTrouble {
+  public boolean parrotTrouble(boolean talking, int hour) {
+    return talking && (hour < 7 || hour > 20);
+  }
+}
