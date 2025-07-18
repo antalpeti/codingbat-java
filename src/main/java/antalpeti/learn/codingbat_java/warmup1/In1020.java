02f900404f5b1eb1b69247466865cf472ab73b94
antalpeti <antalpeti@gmail.com>
2025-07-18 07:16:02 +0200

    Add In1020 class and corresponding unit tests for range validation

---
+++ src/main/java/antalpeti/learn/codingbat_java/warmup1/In1020.java
@@ -0,0 +1,11 @@
+package antalpeti.learn.codingbat_java.warmup1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class In1020 {
+    public boolean in1020(int a, int b) {
+        return (a >= 10 && a <= 20) || (b >= 10 && b <= 20);
+    }
+}
+
